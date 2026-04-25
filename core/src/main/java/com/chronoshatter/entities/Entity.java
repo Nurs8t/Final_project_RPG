693545d17bcd1b255254c1af19030610ae597720
Nurs8t <nurseiit.atystanbek@narxoz.kz>
2026-04-25 17:51:01 +0500

    add Entity

--- core/src/main/java/com/chronoshatter/entities/Entity.java
+++ core/src/main/java/com/chronoshatter/entities/Entity.java
@@ -1,4 +1,37 @@
 package com.chronoshatter.entities;
 
-public class Entity {
+import com.chronoshatter.entities.components.Component;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Entity {
+
+    private static int idCounter = 0;
+
+    private final int id;
+    private final Map<Class<? extends Component>, Component> components;
+
+    public Entity() {
+        this.id         = idCounter++;
+        this.components = new HashMap<>();
+    }
+
+    public <T extends Component> void addComponent(T component) {
+        components.put(component.getClass(), component);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Component> T getComponent(Class<T> type) {
+        return (T) components.get(type);
+    }
+
+    public <T extends Component> boolean hasComponent(Class<T> type) {
+        return components.containsKey(type);
+    }
+
+    public int getId() { return id; }
+
+    public abstract void update(float delta);
+    public abstract void render(com.badlogic.gdx.graphics.g2d.SpriteBatch batch);
+    public abstract void dispose();
 }
