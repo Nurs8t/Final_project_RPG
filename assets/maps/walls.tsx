18ee6c435c19ec7f1afe00b94b4c238d26a16dc0
dulatomurtay <dulat.omurtai@narxoz.kz>
2026-05-01 10:00:00 +0500

    Add all game assets: sprites, maps, sounds

---
+++ assets/maps/walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="walls" tilewidth="16" tileheight="16" tilecount="6032" columns="104">
+ <image source="map2.png" width="1672" height="941"/>
+</tileset>
